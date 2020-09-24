e5476b6195c75114aed53ba1508e8508eab4c3a5
AryenN <70106602+AryenNN@users.noreply.github.com>
2020-09-24 18:13:06 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+DashcamConfig = {}
+
+DashcamConfig.useMPH = false -- False, onu KMH'ye çevirecek
+
+DashcamConfig.RestrictVehicles = true
+DashcamConfig.RestrictionType = "class" -- custom / class
+
+DashcamConfig.AllowedVehicles = {
+    "police2",
+}
